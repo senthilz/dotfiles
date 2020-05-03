d02b1e57d18dbee4d54218d6642ad3080354f3d9
Senthil <inbioz@gmail.com>
2020-05-03 21:11:05 +0100

    Added func to update screenshot folder

--- .f.sh
+++ .f.sh
@@ -5,7 +5,23 @@ sef() {
       return 1
     fi
   fi
-  
+
   du -a $1 | awk '{print $2}' | fzf | xargs -o $EDITOR;
 }
 
+
+screenshot_folder() {
+  case $1 in
+    create)
+      local sd=$HOME/Pictures/screenshots/$(date '+%F-%a');
+      mkdir -p $sd 
+      defaults write com.apple.screencapture location $sd
+      ;;
+    jpg)
+      defaults write com.apple.screencapture type jpg
+      ;;
+    *)
+      defaults read com.apple.screencapture 
+
+    esac
+  }
