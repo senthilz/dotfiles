6afd2c08a190a32381b7f26b01e4f53eea6d755d
Senthil <inbioz@gmail.com>
2024-04-07 12:09:47 +0100

    Switched to submodules & refator tmux conf

--- sh/startup.sh
+++ sh/startup.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 if type nvim > /dev/null 2>&1; then
   alias vim='nvim'
 fi
--- sh/util.sh
+++ sh/util.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 sef() { 
   if [[ $# -gt 0 ]]; then
     if [[ ! -d $1 ]]; then
@@ -5,23 +7,22 @@ sef() {
       return 1
     fi
   fi
-
-  du -a $1 | awk '{print $2}' | fzf | xargs -o $EDITOR;
+  du -a "$1" | awk '{print $2}' | fzf | xargs -o "$EDITOR"
 }
 
-
+# manage screen shots folder on mac
 screenshot_folder() {
   case $1 in
     create)
-    local sd=$HOME/Pictures/screenshots/$(date '+%F-%a');
-  mkdir -p $sd 
-    defaults write com.apple.screencapture location $sd
-    ;;
-  jpg)
-    defaults write com.apple.screencapture type jpg
-    ;;
-  *)
-    defaults read com.apple.screencapture 
-
-    esac
+      df=$(date '+%F-%a')
+      local sd="$HOME/Pictures/screenshots/$df"
+      mkdir -p "$sd" 
+      defaults write com.apple.screencapture location "$sd"
+      ;;
+    jpg)
+      defaults write com.apple.screencapture type jpg
+      ;;
+    *)
+      defaults read com.apple.screencapture 
+  esac
 }
